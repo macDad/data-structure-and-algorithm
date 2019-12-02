663e12cd8ebb6207ec8a0eb78f39ab97ff94be71
MadukaJ <jaya.maduka@gmail.com>
2019-12-02 21:07:45 +0800

    Init commit

---
+++ src/main/java/com/mac/dna/DataStructureAndAlgorithmApplication.java
@@ -0,0 +1,13 @@
+package com.mac.dna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStructureAndAlgorithmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataStructureAndAlgorithmApplication.class, args);
+	}
+
+}
